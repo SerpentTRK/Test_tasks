ae14fcdc0bf56b0263bd5fd39163729ab79ba266
SerpentTRK <for.serpent@gmail.com>
2024-08-05 21:39:47 +0300

    тут все считалось верно, но только для целых чисел. Пришлось лезть в ста-а-арые конспекты, и искать описание Decimal. Сейчас вроде и дробные считаются верно

--- task2/task2.py
+++ task2/task2.py
@@ -2,24 +2,27 @@ import sys
 import os
 import math
 
-
-# def read_circle_data(file_path):
-#     with open(file_path, 'r') as file:
-#         data = file.readline().strip().split()
-#         center_x, center_y = int(data[0]), int(data[1])
-#         radius = int(file.readline().strip())
-#     return center_x, center_y, radius
+from decimal import Decimal, getcontext
+getcontext().prec = 40
 
 
+# # def read_circle_data(file_path):
+# #     with open(file_path, 'r') as file:
+# #         data = file.readline().strip().split()
+# #         center_x, center_y = int(data[0]), int(data[1])
+# #         radius = int(file.readline().strip())
+# #     return center_x, center_y, radius
+#
+#
 def read_circle_data(file_path):
     with open(file_path, 'r') as file:
         data = file.read().strip().split()
         if len(data) == 3:
-            center_x, center_y = int(data[0]), int(data[1].replace('\\n', ''))
-            radius = int(data[2])
+            center_x, center_y = Decimal(data[0]), Decimal(data[1].replace('\\n', ''))
+            radius = Decimal(data[2])
         else:
-            center_x, center_y = int(data[0]), int(data[1].split('\\')[0])
-            radius = int(data[1].split('n')[-1])
+            center_x, center_y = Decimal(data[0]), Decimal(data[1].split('\\')[0])
+            radius = Decimal(data[1].split('n')[-1])
     return center_x, center_y, radius
 
 
@@ -31,7 +34,7 @@ def read_points(file_path):
         for line in data:
             line_list = line.strip().split()
             try:
-                x, y = int(line_list[0]), int(line_list[1])
+                x, y = Decimal(line_list[0]), Decimal(line_list[1])
                 points.append((x, y))
             except IndexError:
                 pass
@@ -41,21 +44,22 @@ def calculate_distance_between_center_and_point(x1, y1, x2, y2):
     """
     формулу расстояния между двумя точками в декартовой системе координат
     """
-    # print(f"Между двумя точками расстояние {math.sqrt((x2 - x1)**2 + (y2 - y1)**2)}")
-    return math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+    # print(math.sqrt((x2 - x1)**2 + (y2 - y1)**2), radius)
+    # return math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+    return Decimal((x2 - x1) ** 2 + (y2 - y1) ** 2).sqrt()
 
 def point_position(center_x, center_y, radius, x, y):
     distance = calculate_distance_between_center_and_point(center_x, center_y, x, y)
-    if distance == radius:
+    tolerance = Decimal('1E-40')  # Установите желаемую точность
+    if abs(radius-distance) < tolerance:
         return 0
     elif distance < radius:
         return 1
     else:
         return 2
 
-
 """
-Никогда с подобными постановками задачи не сталкивался. 
+Никогда с подобными постановками задачи не сталкивался.
 Я не понимаю, как точно Вы собираетесь передавать аргумент, потому делаю два варианта. Через терминал строкой с
 относительными путями, или интерактивно, с полными путями (у меня тут пути для себя сохранены, гонять тесты)
 """
@@ -74,3 +78,6 @@ points = read_points(points_file_path)
 for point in points:
     position = point_position(center_x, center_y, radius, point[0], point[1])
     print(position)
+
+
+
